599b76957374df35a7c910c54e55dc2c124cea30
Alex Dinger <alexdinger@gmail.com>
2021-07-15 16:24:27 -0400

    Added filted to element and adjusted size

--- src/components/Element.jsx
+++ src/components/Element.jsx
@@ -11,7 +11,7 @@ const Element = (props) => {
     <div
       className={` ${
         cardState ? "" : ""
-      }  rounded-sm border-2 border-transparent hover:border-black cursor-pointer overflow-hidden`}
+      }  rounded-sm border-2 border-transparent cursor-pointer overflow-hidden`}
       data-id={props.id}
       onClick={() => setCardState(!cardState)}
     >
@@ -22,14 +22,14 @@ const Element = (props) => {
       )}
 
       {
-          // Element Wrapper
+        // Element Wrapper
       }
       <div
         className={`${
           cardState
-            ? "absolute inset-5 space-x-5 -translate-x-1/2 -translate-y-1/2 z-50 p-3 "
-            : "static translate-x-0 translate-y-0 z-0 p-0 space-x-0"
-        }  flex flex-row justify-between items-center transform top-1/2 left-1/2`}
+            ? "absolute inset-5 space-x-5 -translate-x-1/2 -translate-y-1/2 z-50 p-3 hover:scale-100 filter-none"
+            : "static translate-x-0 translate-y-0 z-0 p-0 space-x-0 hover:scale-105"
+        }  flex flex-row justify-between items-center transform transition duration-300 top-1/2 left-1/2 antialiased outline-white w-24 h-24`}
       >
         <ElementIcon {...props} cardState={cardState} />
         {cardState ? <ElementCard cardState={cardState} /> : ""}
